9003eb57b2792bd2dde55b83e4dbc42fcc6a4367
Tomas Axerot <tomas.axerot@gmail.com>
2017-03-12 22:26:21 +0100

    Updated multi sensor with a main state and some consmetic updates

--- devicetypes/tomasaxerot/virtual-multi-sensor.src/virtual-multi-sensor.groovy
+++ devicetypes/tomasaxerot/virtual-multi-sensor.src/virtual-multi-sensor.groovy
@@ -14,12 +14,15 @@
 metadata {
 	definition (name: "Virtual Multi Sensor", namespace: "tomasaxerot", author: "Tomas Axerot") {
 		capability "Motion Sensor"
-        capability "Contact Sensor"
+        capability "Contact Sensor"        
 
 		command "active"
 		command "inactive"
         command "open"
 		command "close"
+        
+        command "localOk"
+        command "localWarn"
 	}
 
 	simulator {
@@ -29,21 +32,26 @@ metadata {
 		status "closed": "contact:closed"
 	}
     
-    multiAttributeTile(name:"motionTile", type:"generic", width:6, height:4) {
-    	tileAttribute("device.motion", key: "PRIMARY_CONTROL") {
-        	attributeState("active", label:'motion', icon:"st.motion.motion.active", backgroundColor:"#53a7c0")
-            attributeState("inactive", label:'no motion', icon:"st.motion.motion.inactive", backgroundColor:"#ffffff")            
-    	}
+    tiles(scale: 2) {
+        multiAttributeTile(name:"mainTile", type: "generic", width: 6, height: 4, canChangeIcon: false) {
+            tileAttribute ("device.virtualMultiSensor", key: "PRIMARY_CONTROL") {
+                attributeState "ok", label: 'ok', icon: "st.Home.home3", backgroundColor: "#79b821"
+                attributeState "warn", label: 'warn', icon: "st.alarm.alarm.alarm", backgroundColor: "#8a0707"
+            }
+        }
+
+        standardTile("motionTile", "device.motion", width:3, height:2) {
+        	state("active", label:'motion', icon:"st.motion.motion.active", backgroundColor:"#53a7c0")
+            state("inactive", label:'no motion', icon:"st.motion.motion.inactive", backgroundColor:"#ffffff")                        
+        }
+        standardTile("contactTile", "device.contact", width:3, height:2) {
+        	state("open", label:'${name}', icon:"st.contact.contact.open", backgroundColor:"#ffa81e")
+            state("closed", label:'${name}', icon:"st.contact.contact.closed", backgroundColor:"#79b821")                                 
+        }        
+
+        main(["mainTile"])        
+        details(["mainTile", "motionTile", "contactTile"])
     }
-    multiAttributeTile(name:"contactTile", type:"generic", width:6, height:4) {
-    	tileAttribute("device.contact", key: "PRIMARY_CONTROL") {
-        	attributeState("open", label:'${name}', icon:"st.contact.contact.open", backgroundColor:"#ffa81e")
-			attributeState("closed", label:'${name}', icon:"st.contact.contact.closed", backgroundColor:"#79b821")                     
-    	}
-    }
-    
-    main(["motionTile"])
-	details(["motionTile", "contactTile"])
 }
 
 def parse(String description) {
@@ -55,19 +63,49 @@ def parse(String description) {
 def active() {
 	log.trace "active"
 	sendEvent(name: "motion", value: "active")
+    updateMainState()    
 }
 
 def inactive() {
 	log.trace "inactive"
     sendEvent(name: "motion", value: "inactive")
+    updateMainState()
 }
 
 def open() {
 	log.trace "open"
 	sendEvent(name: "contact", value: "open")
+    updateMainState()
 }
 
 def close() {
 	log.trace "close"
     sendEvent(name: "contact", value: "closed")
+    updateMainState()
+}
+
+def updated() {
+	log.debug "updated"
+    updateMainState()    
+}
+
+def updateMainState() {
+	if(device.currentValue("motion") == "active" || device.currentValue("contact") == "open")
+    	localWarn()
+    else
+    	localOk()
+}
+
+def localOk() {
+	if(device.currentValue("virtualMultiSensor") != "ok") {
+    	log.trace "localOk"
+		sendEvent(name: "virtualMultiSensor", value: "ok", displayed: false)
+    }
+}
+
+def localWarn() {
+	if(device.currentValue("virtualMultiSensor") != "warn") {
+		log.trace "localWarn"
+		sendEvent(name: "virtualMultiSensor", value: "warn", displayed: false)
+    }
 }
