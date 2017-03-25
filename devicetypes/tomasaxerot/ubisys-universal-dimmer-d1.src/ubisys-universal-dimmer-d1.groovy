c2665b02d2bce0c0e73b6eb11381e3897a2f9401
Tomas Axerot <tomas.axerot@gmail.com>
2017-03-25 22:32:49 +0100

    D1 DTH: Added configuration and possibility to set phase control, also logging Dimmer setup(capabilities, status and mode) on refresh

--- devicetypes/tomasaxerot/ubisys-universal-dimmer-d1.src/ubisys-universal-dimmer-d1.groovy
+++ devicetypes/tomasaxerot/ubisys-universal-dimmer-d1.src/ubisys-universal-dimmer-d1.groovy
@@ -17,6 +17,7 @@
   * I would consider this DTH in alpha state atm
   * 
   * Version: 
+  * 0.2 (2017-03-25): Added configuration and possibility to set phase control, also logging Dimmer setup(capabilities, status and mode) on refresh
   * 0.1 (2017-03-24): Initial version of DTH for Ubisys Universal Dimmer D1 (Rev. 1), Application: 1.12, Stack: 1.88, Version: 0x010C0158
   * 
   */
@@ -42,17 +43,18 @@ metadata {
         //0301 = ballast
         //0702 = Metering
 		//0B04 = Electrical Measurement
-        //FC01 = Manufacturer configuration
+        //FC01 = Dimmer Setup, Manufacturer configuration
 
 		//Raw
 		//01 0104 0101 00 08 0000 0003 0004 0005 0006 0008 0301 FC01 00
         //model: D1 (5503), manufacturer: ubisys
+        //Dimmable Light (0x0101)
         //Endpoint 1: in: 0000, 0003, 0004, 0005, 0006, 0008, 0301 FC01        
         //Endpoint 2: in: 0000, 0003, out: 0005, 0006, 0008 (input 1)
         //Endpoint 3: in: 0000, 0003, out: 0005, 0006, 0008 (input 2)
         //Endpoint 4: in: 0702, 0B04        
         
-        fingerprint profileId: "0104", inClusters: "0000, 0003, 0004, 0005, 0006, 0008, 0702", manufacturer: "ubisys", model: "D1 (5503)", deviceJoinName: "Ubisys Universal Dimmer D1"
+        fingerprint profileId: "0104", inClusters: "0000, 0003, 0004, 0005, 0006, 0008, 0702", outClusters: "0005, 0006, 0008", manufacturer: "ubisys", model: "D1 (5503)", deviceJoinName: "Ubisys Universal Dimmer D1"
     }
 
     tiles(scale: 2) {
@@ -70,11 +72,29 @@ metadata {
                 attributeState "power", label:'${currentValue} W'
             }
         }
+        
         standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
             state "default", label:"", action:"refresh.refresh", icon:"st.secondary.refresh"
         }
+        
+        standardTile("configure", "device.configure", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
+			state "configure", label:'', action:"configuration.configure", icon:"st.secondary.configure"
+		}
+        
         main "switch"
-        details(["switch", "refresh"])
+        details(["switch", "refresh", "configure"])
+    }
+    
+    preferences {
+        //input name: "email", type: "email", title: "Email", description: "Enter Email Address", required: true, displayDuringSetup: true
+        //input name: "text", type: "text", title: "Text", description: "Enter Text", required: true
+        //input name: "number", type: "number", title: "Number", description: "Enter number", required: true
+        //input name: "bool", type: "bool", title: "Bool", description: "Enter boolean", required: true
+        //input name: "password", type: "password", title: "password", description: "Enter password", required: true
+        //input name: "phone", type: "phone", title: "phone", description: "Enter phone", required: true
+        //input name: "decimal", type: "decimal", title: "decimal", description: "Enter decimal", required: true
+        //input name: "time", type: "time", title: "time", description: "Enter time", required: true
+        input name: "phaseControl", type: "enum", title: "Phase Control", options: ["Auto", "Leading", "Trailing"], description: "Enter Phase Control, Auto is recommended", required: false
     }
 }
 
@@ -87,28 +107,92 @@ def parse(String description) {
     	log.trace "parse: event is $event.name"
 		if (event.name == "power") {			
             def descriptionText = '{{ device.displayName }} power is {{ value }} Watts'
-			event = createEvent(name: event.name, value: event.value, descriptionText: descriptionText, translatable: true)
+			return createEvent(name: event.name, value: event.value, descriptionText: descriptionText, translatable: true)
 		} 
-	} else {
-		def cluster = zigbee.parse(description)
+        
+        return createEvent(event)        
+	} 
+    
+    def cluster = zigbee.parse(description)
+    if(cluster) {
+    	log.trace "parse: cluster is $cluster"
 
 		if (cluster && cluster.clusterId == 0x0006 && cluster.command == 0x07) {
 			if (cluster.data[0] == 0x00) {
 				log.debug "ON/OFF REPORTING CONFIG RESPONSE: " + cluster
-				event = createEvent(name: "checkInterval", value: 60 * 12, displayed: false, data: [protocol: "zigbee", hubHardwareId: device.hub.hardwareID])
+				return createEvent(name: "checkInterval", value: 60 * 12, displayed: false, data: [protocol: "zigbee", hubHardwareId: device.hub.hardwareID])
 			} else {
 				log.warn "ON/OFF REPORTING CONFIG FAILED- error code:${cluster.data[0]}"
-				event = null
+				return null
 			}
-		} else if(!shouldProcessMessage(cluster)) {
-        	log.trace "Message ignored"
-            event = null        	
+		} else if(cluster && !shouldProcessMessage(cluster)) {
+        	log.trace "parse: cluster message ignored"
+            return null        	
         } else {
-			log.warn "Failed to process message: $description"
+			log.warn "parse: failed to process cluster message"
 			log.debug "${cluster}"
-		}        
-	}
-	return event ? createEvent(event) : event   
+            return null
+		}
+    }
+    
+    def map = zigbee.parseDescriptionAsMap(description)
+    if(map) {
+    	log.trace "parse: map is $map"
+        
+    	if (map.clusterInt == 0xFC01 && map.attrInt == 0x0000 && map.value) {        	
+            def capabilitiesInt = Integer.parseInt(map.value, 16)
+            /* 8-bit bitmap	BITMAP8	0x18
+            Forward Phase Control #0 (0x01) When this bit is set, the dimmer supports AC forward phase control.
+			Reverse Phase Control #1 (0x02) When this bit is set, the dimmer supports AC reverse phase control.
+			RFU #2…#4 (0x1C) These bits are reserved for future use and must be written as 0 and ignored when read.
+			Reactance Discriminator #5 (0x20) When this bit is set, the dimmer is capable of measuring the reactance of 
+            	the attached ballast good enough to distinguish inductive and capacitive loads and select the appropriate dimming technique accordingly.
+			Configurable Curve #6 (0x40) When this bit is set, the dimmer is capable of replacing the built-in, default
+            	dimming curve, with a curve that better suits the attached ballast
+			Overload detection #7 (0x80) When this bit is set, the dimmer is capable of detecting an output overload
+            	and shutting the output off to prevent damage to the dimmer.
+            */            
+            if(capabilitiesInt & 0x01)
+            	log.trace "parse: Forward Phase Control supported"                
+            if(capabilitiesInt & 0x02)
+            	log.trace "parse: Reverse Phase Control supported"
+        }
+        else if (map.clusterInt == 0xFC01 && map.attrInt == 0x0001 && map.value) {        	
+            def statusInt = Integer.parseInt(map.value, 16)
+            /* 8-bit bitmap	BITMAP8	0x18
+            Forward Phase Control #0 (0x01) When this bit is set, the dimmer is currently operating in AC forward phase control mode.
+            Reverse Phase Control #1 (0x02) When this bit is set, the dimmer is currently operating in AC reverse phase control.
+            Operational #2 (0x04) These bits are reserved for future use and must be written as 0 and ignored when read.
+            Overload #3 (0x08) The output is currently turned off, because the dimmer has detected an overload.
+            RFU #4…#5 (0x30) Reserved for future use. Set to 0 when writing, ignore when reading.
+            Capacitive Load #6 (0x40) When this bit is set, the dimmer’s reactance discriminator has detected a capacitive load.
+            Inductive Load #7 (0x80) When this bit is set, the dimmer’s reactance discriminator has detected an inductive load.
+            */            
+            if(statusInt & 0x01)
+            	log.trace "parse: operating in forward phase (Leading) control mode"                
+            if(statusInt & 0x02)
+            	log.trace "parse: operating in reverse phase (Trailing) control mode"                
+            if(statusInt & 0x08)
+            	log.trace "parse: overload"
+            if(statusInt & 0x40)
+            	log.trace "parse: capacitive load detected"
+            if(statusInt & 0x80)
+            	log.trace "parse: inductive load detected"            
+        }
+        else if (map.clusterInt == 0xFC01 && map.attrInt == 0x0002 && map.value) {
+        	/*
+            Phase Control #0…#1 (0x02) Specifies the mode of operation:
+			00b: Automatically select the appropriate dimming technique
+			01b: Always use forward phase control (leading edge, L)
+			02b: Always use reverse phase control (trailing edge, C/R)
+			11b: Reserved. Do not use.
+            */
+            log.trace "parse: mode is: $map.value"
+        }    
+    }
+    
+    log.warn "parse: failed to process message"	
+    return null
 }
 
 private boolean shouldProcessMessage(cluster) {
@@ -150,7 +234,15 @@ def refresh() {
     //Removed: zigbee.electricMeasurementPowerRefresh(), zigbee.electricMeasurementPowerConfig()
     //zigbee.readAttribute(0x0702, 0x0400, [destEndpoint: 0x04]) /*+ zigbee.readAttribute(0x0B04, 0x050B, [destEndpoint: 0x04])*/
     
-    zigbee.onOffRefresh() + zigbee.levelRefresh() + zigbee.readAttribute(0x0702, 0x0400, [destEndpoint: 0x04])
+    def refreshCmds = []
+    refreshCmds += zigbee.onOffRefresh() +
+    			   zigbee.levelRefresh() +
+                   zigbee.readAttribute(0x0702, 0x0400, [destEndpoint: 0x04]) +
+                   zigbee.readAttribute(0xFC01, 0x0000) + 
+                   zigbee.readAttribute(0xFC01, 0x0001) +
+                   zigbee.readAttribute(0xFC01, 0x0002)
+                
+    return refreshCmds
 }
 
 def configure() {
@@ -163,5 +255,25 @@ def configure() {
     
     //zigbee.configureReporting(0x0702, 0x0400, 0x2A, 1, 600, 0x05, [destEndpoint: 0x04]) /*+ zigbee.configureReporting(0x0B04, 0x050B, 0x29, 1, 600, 0x05, [destEndpoint: 0x04])*/
     
-    refresh() + zigbee.onOffConfig(0, 300) + zigbee.levelConfig() + zigbee.configureReporting(0x0702, 0x0400, 0x2A, 1, 600, 0x05, [destEndpoint: 0x04])
+    
+    def configCmds = []
+    
+    //Configure phase control
+    if(phaseControl) {
+    	log.debug "configure: set phase control to $phaseControl"
+        if(phaseControl == "Auto") {        	
+            configCmds += zigbee.writeAttribute(0xFC01, 0x0002, 0x18, 0x00) //Auto
+        } else if(phaseControl == "Leading") {        	
+        	configCmds += zigbee.writeAttribute(0xFC01, 0x0002, 0x18, 0x01) //Leading        	
+        } else if(phaseControl == "Trailing") {        	
+            configCmds += zigbee.writeAttribute(0xFC01, 0x0002, 0x18, 0x02) //Trailing
+        }    	
+    }
+    
+    //Reporting
+    configCmds += zigbee.onOffConfig(0, 300) + 
+    			  zigbee.levelConfig() + 
+                  zigbee.configureReporting(0x0702, 0x0400, 0x2A, 1, 600, 0x05, [destEndpoint: 0x04]) 
+    
+     return refresh() + configCmds
 }
